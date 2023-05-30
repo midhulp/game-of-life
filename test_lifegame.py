d71bba2965b178631871a731b52f292099d565c8
midhul <dhulmi3@gmail.com>
2023-05-31 01:18:14 +0530

    GREENAdded test to collect grid from input

--- test_lifegame.py
+++ test_lifegame.py
@@ -4,11 +4,9 @@ import lifegame
 def test_show_display():
     assert lifegame.display()=="Welcome to game of life"
 
-def test_store_grid():
-    assert lifegame.store_grid([["x","x","x"],["0","0","0"],["x","x","x"]])=="""xxx
-000
-xxx"""
-                                                                    
-
-
-
+          
+def test_collect_grid(monkeypatch):
+    input = ["x,x,x", "0,0,0", "x,x,x"] 
+    monkeypatch.setattr('builtins.input', lambda _: input.pop(0))
+    new_grid = [['x', 'x', 'x'], ['0', '0', '0'], ['x', 'x', 'x']]
+    assert lifegame.collect_grid() == new_grid
