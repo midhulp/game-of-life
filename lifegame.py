38a09d2239e92ad127b48079de47aa9b4a769db2
midhul <dhulmi3@gmail.com>
2023-06-01 17:13:28 +0530

    Removed user input function

--- lifegame.py
+++ lifegame.py
@@ -1,18 +1,27 @@
 def display():
-    a=("Welcome to game of life")
+    a=("Welcome to game of life!")
     return a
 
 
-def collect_grid():
-    grid = []
-    for _ in range(3):
-        row = input("Enter a row (('x' or '0') -separated by comma): ")
-        grid.append(row.split(","))
-    return grid
+# def collect_grid():
+#     grid = []
+#     for _ in range(3):
+#         row = input("Enter a row (('1' or '0') -separated by comma): ")
+#         grid.append(row.split(","))
+#     return grid
 
 
-    
-       
+def show_newgrid(grid):
+    newgrid = []
+    for row in grid:
+        val = ''
+        for item in row:
+            if item == 1:
+                val += '1 '
+            else:
+                val += '0 '
+        newgrid.append(val)
+    return '\n'.join(newgrid)
 
 
 
