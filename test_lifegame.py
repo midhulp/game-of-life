08a2c3e93f8db3f088c8c9b68eebd91dcc35ee1e
midhul <dhulmi3@gmail.com>
2023-06-01 19:03:08 +0530

    Adds test to count alive neibhbors

--- test_lifegame.py
+++ test_lifegame.py
@@ -3,13 +3,20 @@ import lifegame
 def test_show_display():
     assert lifegame.display()=="Welcome to game of life!"
 
-# def test_collect_grid(monkeypatch):
-#     input = ["1,1,1", "0,0,0", "1,1,1"] 
-#     monkeypatch.setattr('builtins.input', lambda _: input.pop(0))
-#     new_grid = [['1', '1', '1'], ['0', '0', '0'], ['1', '1', '1']]
-#     assert lifegame.collect_grid() == new_grid
-
 def test_show_newgrid():
     grid=[[1, 1, 1], ['0', '0', '0'], [1, 1, 1]]
     assert lifegame.show_newgrid(grid) == "1 1 1 \n0 0 0 \n1 1 1 "
+
+def test_count_live_neighbors():
+
+    grid=[
+        [0, 1, 0],
+        [1, 0, 1],
+        [0, 1, 1]
+    ]
+    assert lifegame.get_count_live_neighbors(grid, 1, 1) == 5
+    assert lifegame.get_count_live_neighbors(grid, 1, 2) == 3
+
+
+
     
