9fd0ee7daacec65e97da1f81d206c532a183651a
midhul <dhulmi3@gmail.com>
2023-06-01 19:16:08 +0530

    Adds function to count no of neighbors

--- lifegame.py
+++ lifegame.py
@@ -2,15 +2,6 @@ def display():
     a=("Welcome to game of life!")
     return a
 
-
-# def collect_grid():
-#     grid = []
-#     for _ in range(3):
-#         row = input("Enter a row (('1' or '0') -separated by comma): ")
-#         grid.append(row.split(","))
-#     return grid
-
-
 def show_newgrid(grid):
     newgrid = []
     for row in grid:
@@ -23,7 +14,17 @@ def show_newgrid(grid):
         newgrid.append(val)
     return '\n'.join(newgrid)
 
+def get_count_live_neighbors(grid, row, col):
+    live_neighbor_count = 0
+    no_zero_set = [(i, j) for i in range(-1, 2) for j in range(-1, 2) if i != 0 or j != 0]
 
+    for i,j in no_zero_set:
+        neighbor_i = row + i
+        neighbor_j = col + j
 
+        if 0 <= neighbor_i< len(grid) and 0 <= neighbor_j < len(grid[0]):
+            if grid[neighbor_i][neighbor_j] == 1:
+               live_neighbor_count += 1
 
+    return live_neighbor_count
 
